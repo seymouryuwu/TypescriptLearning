d4930c1b8ab294ad226e30293cc4a0317bd5ed51
YuWu-Seymour <78057679+seymouryuwu@users.noreply.github.com>
2021-05-19 17:03:34 +1000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import "./style/index.less";
+
+console.log(123);
+
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+
+module.exports = {
+    entry: "/src/index.ts",
+
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        /*environment: {
+            arrowFunction: false
+        }*/
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                [
+                                    "@babel/preset-env",
+                                    {
+                                        targets: {
+                                            "chrome": "88",
+                                        },
+                                        "corejs": "3",
+                                        "useBuiltIns": "usage"
+                                    }
+                                ]
+                            ],
+                        },
+                    },
+
+                    "ts-loader"
+                ],
+                exclude: /node_modules/
+            },
+
+            {
+                test: /\.less$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "less-loader"
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: "index.html",
+            template: "./src/template/index.html"
+        }),
+
+        new CleanWebpackPlugin(),
+    ],
+
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+
+    mode: "production"
+}
